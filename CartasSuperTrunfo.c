15ce3a30ba350fc94e792ec0f3ca1d4f65c54357
samuelalvesvitor <samuelvitoralves152@gmail.com>
2026-02-24 10:21:45 +0000

    Realizei o desafio do cadastro das cartas Super Trunfo, onde criei um programa em C para cadastrar as informações de uma cidade, como nome, população, área e densidade demográfica. O programa utiliza scanf para entrada de dados e printf para exibir as informações cadastradas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,28 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+    printf("Bem-vindo ao Desafio Super Trunfo - Países!\n");
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char nomeCidade[100];
+  int populacao;
+  float area;
+  float densidade;
 
   // Área para entrada de dados
+  printf("Digite o nome da cidade: ");
+  scanf("%s", nomeCidade);
+  printf("Digite a população da cidade: ");
+  scanf("%d", &populacao);
+  printf("Digite a área da cidade (em km²): ");
+  scanf("%f", &area);
+  densidade = populacao / area;
 
   // Área para exibição dos dados da cidade
+  printf("\nInformações da Cidade:\n");
+  printf("Nome: %s\n", nomeCidade);
+  printf("População: %d\n", populacao);
+  printf("Área: %.2f km²\n", area);
+  printf("Densidade Demográfica: %.2f habitantes/km²\n", densidade);
 
 return 0;
 } 
